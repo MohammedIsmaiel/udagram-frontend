8572e2223bda9a77302a8bf112dfed5fd32b772b
Mohammed_Ismaiel <mohammedismaiel333.z@gmail.com>
2021-08-31 22:15:51 +0200

    new api howt was added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http:/a05dff373b14147cfa27c76f2f77772f-1275824285.eu-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
